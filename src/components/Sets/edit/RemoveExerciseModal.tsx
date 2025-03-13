4526eb5f4baeae462b517e508ecb63133b676f02
Gonzalo Martinez <gonzalovam@hotmail.com>
2025-03-13 02:29:10 -0300

    remove exercise

--- src/components/Sets/edit/RemoveExerciseModal.tsx
+++ src/components/Sets/edit/RemoveExerciseModal.tsx
@@ -5,7 +5,7 @@ import { TypesSet } from "@/types/sets";
 import { showErrorAlert } from "@/utils/SweetAlertUtils";
 import { Button } from "@nextui-org/react";
 // import { useRouter } from "next/router";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { FaDeleteLeft } from "react-icons/fa6";
 
 interface Props {
@@ -21,38 +21,44 @@ interface Props {
 
 const RemoveExerciseModal: React.FC<Props> = ({ set, exerciseId, onRemoveExercise }) => {
     const { user } = useAuth()
+    const [load, setLoad] = useState<boolean>(false);
     // const router = useRouter()
     const handleRemoveExercise = async (exerciseId: string) => {
+        setLoad(true)
         try {
             const response = await fetch(`${url}/set/remove-exercise/${set._id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
                     , Authorization: `Bearer ${user?.token}`
-
+                    
                 },
                 body: JSON.stringify({ exerciseId }),
             });
-
+            
             if (!response.ok) {
                 const error = await response.json();
                 throw new Error(error.message || "Error al actualizar el set");
-
+                
             }
-
+            setLoad(false)
+            const data = await response.json();
+            
+            onRemoveExercise(data.exercises)
+            
             // Redirigir o mostrar un mensaje de éxito
-            onRemoveExercise(prev => prev.filter(e => e.exercise._id !== exerciseId))
             // showSuccessAlert({ title: "Ejercicio eliminado del set", text: "" })
             // router.reload()
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
+            setLoad(false)
             console.error(error);
             showErrorAlert(error.message)
         }
     };
 
 
-    return (<> <Button variant="light" color="danger" className={`min-w-0`} onPress={() => handleRemoveExercise(exerciseId)}>
+    return (<> <Button variant="light" color="danger" isLoading={load} className={`min-w-0`} onPress={() => handleRemoveExercise(exerciseId)}>
         <FaDeleteLeft />
     </Button></>);
 }
